edad0bc2d7d96da44391963e4616177cf25ad2e1
houssem <houssemranah@gmail.com>
2022-04-07 16:01:49 +0100

    add background

--- lib/Core/constants.dart
+++ lib/Core/constants.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-Color mainColor = Color(0xE13D3B);
-Color inColor = Color(0xFFFFFF);
-Color backColor = Color(0xE5E5E5);
-Color frontColor = Color(0x000000);
-Color greyColor = Color(0xC4C4C4);
+Color mainColor = Color(0xFFE13D3B);
+Color inColor = Color(0xFFFFFFFF);
+Color backColor = Color(0xFFf5f5f5);
+Color frontColor = Color(0xFF000000);
+Color greyColor = Color(0xFFC4C4C4);
 
 TextStyle textStyleSmall = TextStyle(fontSize: 10.0);
 TextStyle textStyleSouSimple = TextStyle(fontSize: 13.0);
@@ -12,6 +12,6 @@ TextStyle textStyleSimple = TextStyle(fontSize: 15.0);
 TextStyle textStyleSouTitle =
     TextStyle(fontSize: 17.0, fontWeight: FontWeight.w500);
 TextStyle textStyleTitle =
-    TextStyle(fontSize: 20.0, fontWeight: FontWeight.w900);
+    TextStyle(fontSize: 30.0, fontWeight: FontWeight.w900);
 
 BorderRadius radius = BorderRadius.all(Radius.circular(30));
--- lib/UI/Screens/login.dart
+++ lib/UI/Screens/login.dart
@@ -14,41 +14,89 @@ class _LoginScreenState extends State<LoginScreen> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: backColor,
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Welcome to Maclat, a new food expernience',
-              style: textStyleTitle,
-            ),
-            Container(
-              padding: EdgeInsets.all(8.0),
-              width: MediaQuery.of(context).size.width * 0.8,
-              decoration:
-                  BoxDecoration(borderRadius: radius, color: Colors.white),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'continu with your phone number',
-                    style: textStyleSimple,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                        prefix: Text("+213"),
-                        border: null,
-                        errorBorder: null,
-                        enabledBorder: null,
-                        focusedBorder: null,
-                        disabledBorder: null),
-                  ),
-                ],
-              ),
-            )
-          ],
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        floatingActionButton: Container(
+          decoration: BoxDecoration(
+            borderRadius: radius,
+            color: mainColor,
+          ),
+          padding: EdgeInsets.all(16.0),
+          child: Text(
+            "étape suivante",
+            style: textStyleSouTitle.copyWith(color: inColor),
+          ),
         ),
+        body: Stack(children: [
+          Positioned(
+              top: MediaQuery.of(context).size.height * 0.5,
+              child: Image.asset('phonenumberback.png')),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Row(),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Bienvenue à Maclat',
+                  style: textStyleTitle,
+                ),
+              ),
+              Text(
+                'une expérience culinaire unique',
+                style: textStyleSouTitle,
+              ),
+              Spacer(),
+              Container(
+                padding: EdgeInsets.all(32.0),
+                width: MediaQuery.of(context).size.width * 0.8,
+                decoration:
+                    BoxDecoration(borderRadius: radius, color: Colors.white),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Utilisez votre numéro de téléphone pour accéder aux services de maclat',
+                      style: textStyleSimple,
+                    ),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    Container(
+                      decoration:
+                          BoxDecoration(borderRadius: radius, color: greyColor),
+                      child: Center(
+                        child: TextField(
+                          textAlignVertical: TextAlignVertical.center,
+                          style: textStyleSimple,
+                          cursorColor: mainColor,
+                          decoration: InputDecoration(
+                              hintText: "0X XX XX XX XX",
+                              icon: Container(
+                                  padding: EdgeInsets.all(18.0),
+                                  decoration: BoxDecoration(
+                                      color: mainColor,
+                                      borderRadius: BorderRadius.only(
+                                          topLeft: Radius.circular(30),
+                                          bottomLeft: Radius.circular(30))),
+                                  child: Text(
+                                    "+213",
+                                    style: textStyleSimple.copyWith(
+                                        color: inColor),
+                                  )),
+                              border: InputBorder.none),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Spacer(),
+            ],
+          ),
+        ]),
       ),
     );
   }
