2a272a0d36093a2090cf668de4368343f9106b98
houssem <houssemranah@gmail.com>
2022-04-07 01:59:57 +0100

    add theme : font family

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,14 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      title: 'Maclat',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        fontFamily: 'Poppins',
       ),
-      // A widget which will be started on application startup
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
