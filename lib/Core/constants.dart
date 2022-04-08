d6c7b878d9ab9ec8947facaf16c31ce444c2e477
FlutLab.io <>
2022-04-08 16:10:50 +0000

    Autosave

--- lib/Core/constants.dart
+++ lib/Core/constants.dart
@@ -9,9 +9,7 @@ Color greyColor = Color(0xFFC4C4C4);
 TextStyle textStyleSmall = TextStyle(fontSize: 13.0);
 TextStyle textStyleSouSimple = TextStyle(fontSize: 15.0);
 TextStyle textStyleSimple = TextStyle(fontSize: 17.0);
-TextStyle textStyleSouTitle =
-    TextStyle(fontSize: 18.0, fontWeight: FontWeight.w300);
-TextStyle textStyleTitle =
-    TextStyle(fontSize: 30.0, fontWeight: FontWeight.w900);
+TextStyle textStyleSouTitle = TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500);
+TextStyle textStyleTitle = TextStyle(fontSize: 30.0, fontWeight: FontWeight.w900);
 
 BorderRadius radius = BorderRadius.all(Radius.circular(30));
