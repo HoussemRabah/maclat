b53ec22dabaf491d095e06b367a389f3f370c3ab
houssem <houssemranah@gmail.com>
2022-04-08 03:07:43 +0100

    add swiper

--- lib/UI/Screens/home.dart
+++ lib/UI/Screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -17,27 +18,49 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: backColor,
       appBar: appBarBuilder(),
-      bottomNavigationBar: Container(
-        color: Colors.white,
-        child: SalomonBottomBar(
-          items: [
-            SalomonBottomBarItem(
-                icon: SvgPicture.asset('assets/icons/food.svg'),
-                title: Text("explorer"),
-                selectedColor: mainColor,
-                unselectedColor: greyColor),
-            SalomonBottomBarItem(
-                icon: SvgPicture.asset('assets/icons/ordre.svg'),
-                title: Text("commandes"),
-                selectedColor: mainColor,
-                unselectedColor: greyColor),
-            SalomonBottomBarItem(
-                icon: SvgPicture.asset('assets/icons/account.svg'),
-                title: Text("profil"),
-                selectedColor: mainColor,
-                unselectedColor: greyColor),
-          ],
-        ),
+      bottomNavigationBar: BottomNavigationBarBuilder(),
+      body: Column(
+        children: [
+          Swiper(
+            itemCount: 3,
+            itemBuilder: (BuildContext context, int index) {
+              return Image.network(
+                "https://via.placeholder.com/350x150",
+                fit: BoxFit.fill,
+              );
+            },
+            autoplay: true,
+            pagination: SwiperPagination.dots,
+          )
+        ],
+      ),
+    );
+  }
+
+  Container BottomNavigationBarBuilder() {
+    return Container(
+      decoration: BoxDecoration(
+          color: inColor,
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30), topRight: Radius.circular(30))),
+      child: SalomonBottomBar(
+        items: [
+          SalomonBottomBarItem(
+              icon: SvgPicture.asset('assets/icons/food.svg'),
+              title: Text("explorer"),
+              selectedColor: mainColor,
+              unselectedColor: greyColor),
+          SalomonBottomBarItem(
+              icon: SvgPicture.asset('assets/icons/ordre.svg'),
+              title: Text("commandes"),
+              selectedColor: mainColor,
+              unselectedColor: greyColor),
+          SalomonBottomBarItem(
+              icon: SvgPicture.asset('assets/icons/account.svg'),
+              title: Text("profil"),
+              selectedColor: mainColor,
+              unselectedColor: greyColor),
+        ],
       ),
     );
   }
