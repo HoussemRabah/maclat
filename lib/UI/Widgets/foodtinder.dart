67737df19a7dffc9ed5ab4a8f3c3a1861fb1f334
houssem <houssemranah@gmail.com>
2022-04-13 01:35:44 +0100

    improve things

--- lib/UI/Widgets/foodtinder.dart
+++ lib/UI/Widgets/foodtinder.dart
@@ -2,6 +2,7 @@ import 'package:badges/badges.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:mbh/Core/constants.dart';
+import 'package:mbh/UI/Screens/food.dart';
 
 class FoodTinder extends StatefulWidget {
   const FoodTinder({Key? key}) : super(key: key);
@@ -17,41 +18,47 @@ class _FoodTinderState extends State<FoodTinder> {
       height: MediaQuery.of(context).size.width - 16.0,
       child: Swiper(
         itemBuilder: (BuildContext context, int index) {
-          return Stack(alignment: Alignment.bottomCenter, children: [
-            Badge(
-              animationType: BadgeAnimationType.slide,
-              animationDuration: Duration(seconds: 1),
-              shape: BadgeShape.square,
-              borderRadius: radius,
-              badgeColor: mainColor,
-              badgeContent: Text(
-                "200DA",
-                style: textStyleSimple.copyWith(color: inColor),
-              ),
-              child: Container(
-                clipBehavior: Clip.antiAlias,
-                width: MediaQuery.of(context).size.width - 16.0,
-                height: MediaQuery.of(context).size.width - 16.0,
-                decoration: BoxDecoration(borderRadius: radius),
-                child: Image.asset(
-                  examples[index],
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            Positioned(
-              bottom: 16.0,
-              child: Container(
-                padding: EdgeInsets.all(8.0),
-                decoration:
-                    BoxDecoration(borderRadius: radius, color: mainColor),
-                child: Text(
-                  examplesNames[index],
+          return GestureDetector(
+            onTap: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => FoodScreen()));
+            },
+            child: Stack(alignment: Alignment.bottomCenter, children: [
+              Badge(
+                animationType: BadgeAnimationType.slide,
+                animationDuration: Duration(seconds: 1),
+                shape: BadgeShape.square,
+                borderRadius: radius,
+                badgeColor: mainColor,
+                badgeContent: Text(
+                  "200DA",
                   style: textStyleSimple.copyWith(color: inColor),
                 ),
+                child: Container(
+                  clipBehavior: Clip.antiAlias,
+                  width: MediaQuery.of(context).size.width - 16.0,
+                  height: MediaQuery.of(context).size.width - 16.0,
+                  decoration: BoxDecoration(borderRadius: radius),
+                  child: Image.asset(
+                    examples[index],
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
-            ),
-          ]);
+              Positioned(
+                bottom: 16.0,
+                child: Container(
+                  padding: EdgeInsets.all(8.0),
+                  decoration:
+                      BoxDecoration(borderRadius: radius, color: mainColor),
+                  child: Text(
+                    examplesNames[index],
+                    style: textStyleSimple.copyWith(color: inColor),
+                  ),
+                ),
+              ),
+            ]),
+          );
         },
         itemWidth: MediaQuery.of(context).size.width - 16.0,
         itemHeight: MediaQuery.of(context).size.width - 16.0,
