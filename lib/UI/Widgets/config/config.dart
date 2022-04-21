908c4142643a56f8f50b72f1cb5d0fed466bb631
houssem <houssemranah@gmail.com>
2022-04-21 20:20:52 +0100

    fix bug

--- lib/UI/Widgets/config/config.dart
+++ lib/UI/Widgets/config/config.dart
@@ -30,52 +30,49 @@ class _ConfigSliderState extends State<ConfigSlider> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(16.0, 0, 16.0, 16.0),
-      child: Badge(
-        alignment: Alignment.topCenter,
-        shape: BadgeShape.square,
-        showBadge: (widget.config.prices[getIndex()] != 0),
-        badgeContent: (widget.config.prices[getIndex()] == 0)
-            ? null
-            : Text(
-                "+${widget.config.prices[getIndex()]}DA",
-                style: textStyleSmall.copyWith(color: inColor),
-              ),
-        child: Container(
-          padding: EdgeInsets.all(8.0),
-          height: 69 + 8.0 + 8.0,
-          width: MediaQuery.of(context).size.width - 16.0 * 2,
-          decoration: BoxDecoration(
-              borderRadius: radius, boxShadow: shadows, color: inColor),
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Sqaure(child: widget.config.icon),
-              Expanded(
-                child: SfSlider(
-                    showDividers: true,
-                    interval: 1.0 / (widget.config.points.length - 1),
-                    stepSize: 1.0 / (widget.config.points.length - 1),
-                    showLabels: true,
-                    activeColor: mainColor,
-                    inactiveColor: inColor,
-                    labelPlacement: LabelPlacement.onTicks,
-                    labelFormatterCallback: (value, formateur) {
-                      return "${widget.config.points[getIndexFromValue(value)]}";
-                    },
-                    value: _value,
-                    onChanged: (newValue) {
-                      setState(() {
-                        _value = newValue;
-                      });
-                    }),
-              ),
-              SizedBox(
-                width: 12.0,
-              ),
-            ],
-          ),
+    return Badge(
+      alignment: Alignment.topCenter,
+      shape: BadgeShape.square,
+      showBadge: (widget.config.prices[getIndex()] != 0),
+      badgeContent: (widget.config.prices[getIndex()] == 0)
+          ? null
+          : Text(
+              "+${widget.config.prices[getIndex()]}DA",
+              style: textStyleSmall.copyWith(color: inColor),
+            ),
+      child: Container(
+        padding: EdgeInsets.all(8.0),
+        height: 69 + 8.0 + 8.0,
+        width: MediaQuery.of(context).size.width - 16.0 * 2,
+        decoration: BoxDecoration(
+            borderRadius: radius, boxShadow: shadows, color: inColor),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Sqaure(child: widget.config.icon),
+            Expanded(
+              child: SfSlider(
+                  showDividers: true,
+                  interval: 1.0 / (widget.config.points.length - 1),
+                  stepSize: 1.0 / (widget.config.points.length - 1),
+                  showLabels: true,
+                  activeColor: mainColor,
+                  inactiveColor: inColor,
+                  labelPlacement: LabelPlacement.onTicks,
+                  labelFormatterCallback: (value, formateur) {
+                    return "${widget.config.points[getIndexFromValue(value)]}";
+                  },
+                  value: _value,
+                  onChanged: (newValue) {
+                    setState(() {
+                      _value = newValue;
+                    });
+                  }),
+            ),
+            SizedBox(
+              width: 12.0,
+            ),
+          ],
         ),
       ),
     );
